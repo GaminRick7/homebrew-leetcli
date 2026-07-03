3093c7094d84f853a39b90fe5bbc06183c40f5e5
Raihaan Sandhu <raihaansandhu@gmail.com>
2026-07-02 22:12:07 -0400

    Update to v0.2.2

--- Formula/leetcli.rb
+++ Formula/leetcli.rb
@@ -1,8 +1,8 @@
 class Leetcli < Formula
   desc "Modern command-line tool for interacting with LeetCode"
   homepage "https://github.com/GaminRick7/leetcli"
-  url "https://github.com/GaminRick7/leetcli/archive/refs/tags/v0.2.1.tar.gz"
-  sha256 "ffa1c4f946e350d079dff1e2f853ce04268097ea57725435bb0ead3cff9740bb"
+  url "https://github.com/GaminRick7/leetcli/archive/refs/tags/v0.2.2.tar.gz"
+  sha256 "34f1d4e245f09b1eb8e558a9bd38528bc49c19587b11e5245f6fd65a578dcb08"
   license "MIT"
   head "https://github.com/GaminRick7/leetcli.git", branch: "main"
 
